b3c1f130a78581d10aea13f44e276ae5c09a34fc
mahir24 <37030837+mahir24@users.noreply.github.com>
2021-12-23 18:06:09 -0800

    Update to actually send tokens
    Smart Contract actually functional and sends tokens to specified addresses upon creator's death.

--- will.sol
+++ will.sol
@@ -29,6 +29,32 @@ contract Will {
     //create a list(array) for the addresses that will inherit the LOOT
     address payable[] familyWallets; //array of addresses that can send/recieve eth
     
-    //key store value -- using Mapping
-    
+    //key store value -- using mapping
+    mapping(address => uint) inheritance; //each address should have its own uint -- mapping through the inheritance
+
+    //set inheritance for each addresses
+    function setInheritance(address payable wallet, uint amount) public onlyOwner{ //only the owner can run this function
+        //add wallet to family wallets
+        familyWallets.push(wallet); //push the parameter wallet address into the familyWallets array
+        inheritance[wallet] = amount; //maps the inheritance amount to each wallet
+    }
+
+    //pay each family member based on wallet address
+    function payout() private mustBeDeceased { //using a dev created modifier to only execute when gramps is dead
+        
+        for (uint i=0; i <familyWallets.length; i++) {
+            //.transfer takes in the amount as an argument 
+            familyWallets[i].transfer(inheritance[familyWallets[i]]); //transferring funds from contract address to reciever address
+        }
+
+    }
+
+    //if it was a real world smart contract it would connect to an Oracle to check if gramps died or not
+    //Oracle sqitch simulation
+    function isDeceased() public onlyOwner {
+        deceased = true;
+
+        payout(); // payout after deceased is confirmed
+    } 
+
 }
