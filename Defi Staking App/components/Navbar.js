9bd0aaf157c8755faab96c1e9b0d53d0f984ccca
mahir24 <37030837+mahir24@users.noreply.github.com>
2022-08-06 10:46:28 -0700

    Update Navbar.js
    bring in account number from metamask
    prettify the navbar

--- Defi Staking App/components/Navbar.js
+++ Defi Staking App/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react' 
+import bank from '../bank.png'
 
 
 class Navbar extends Component {
@@ -6,11 +7,15 @@ class Navbar extends Component {
     render() {
         return (
             <nav className='navbar navbar-dark fixed-top shadow p-0' style = {{backgroundColor:'black', height: '50px'}}>
-                <a style={{color: 'white'}}> DAPP Yield Staking (Decentralized Banking)</a>
-                <ul>
-                    <li>
+                <a className= ' navbar-brand col-sm-3 col-md-2 mr-0' style={{color: 'white'}}>
+                    <img src={bank} width='50' height='30' className='d-inline-block align-top' alt='bank'/>
+                    &nbsp;
+                    DAPP Yield Staking (Decentralized Banking)                
+                </a>
+                <ul className='navbar-nav px-3'>
+                    <li className= ''>
                         <small style ={{color:'white'}}>
-                            Account Number
+                            ACCOUNT NUMBER: {this.props.account}
                         </small>
                     </li>
                 </ul>
